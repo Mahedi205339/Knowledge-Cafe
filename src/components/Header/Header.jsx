cb7fe562d6d3aca721452caa4a9433a6b522c35f
mahedi205339 <mahedi205339gmaol.com>
2023-09-13 14:07:53 +0600

    added blog jsx file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
+import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <div>
+        <div className='w-4/5 mx-auto mt-3 flex justify-between items-center border-b-2'>
             <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+            <img src={profile} alt="" />
         </div>
     );
 };
